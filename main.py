8f9090c191caf4406c68b5cff061a0fa58aec2b8
Yousefrahimi96 <yousefrahimi310@gmail.com>
2025-03-29 15:42:09 +0330

    pep8 standard is done

--- main.py
+++ main.py
@@ -3,26 +3,34 @@ import random
 doors = ['goat', 'car', 'goat']
 
 def monty_hall(switch):
+    """Simulate the Monty Hall problem with an option to switch doors.
+    
+    Args:
+        switch (bool): Whether to switch the initial choice or not.
+    
+    Returns:
+        bool: True if the car is won, False otherwise.
+    """
     random.shuffle(doors)
     initial_choice = random.choice(range(3))
-    open_door = [i for i in range(3) if i != initial_choice and doors[i] == 'goat'][0]
+    revealed_door = [i for i in range(3) if i != initial_choice and doors[i] == 'goat'][0]
     
     if switch:
-        final_choice = [n for n in range(3) if n != initial_choice and n != open_door][0]
+        final_choice = [n for n in range(3) if n != initial_choice and n != revealed_door][0]
     else:
         final_choice = initial_choice
         
     return doors[final_choice] == 'car'
 
 
-def count_resault(count):
-    resuly_count = []
+def count_result(count):
+    result_count = []
     for _ in range(count):
-        resuly_count.append(monty_hall(True))
+        result_count.append(monty_hall(True))
 
-    resault = resuly_count.count(True)/count*100
-    print(resault)
+    result = result_count.count(True) / count * 100
+    print(result)
 
 if __name__ == '__main__':
-    count = int(input('How many do you want to repeat the geame: '))
-    count_resault(count)
+    count = int(input('How many do you want to repeat the game: '))
+    count_result(count)
