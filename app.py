ea1a6686ff2fe613e0e214fee3319358b00fc569
Yousefrahimi96 <yousefrahimi310@gmail.com>
2025-04-05 07:18:24 +0330

    test wins with switching is done

--- app.py
+++ app.py
@@ -1,4 +1,24 @@
+import streamlit as st
 import random
+import time
+
+#title of application
+st.image("pic.png")
+
+count = st.number_input("Enter number of games to simulate", min_value=1, value=100)
+
+#Create two list to hold win percentage for hold cases
+wins_no_switch = 0
+win_switch = 0
+
+col1, col2 = st.columns(2)
+col1.subheader("Win Percentage Without Switching")
+chart1 = col1.line_chart(x=None, y=None, width=0, height=400)
+chart1.add_rows([1.0]) # Add a row to ensure the chart y-axis starts at 1
+col2.subheader("Win Percentage With Switching")
+chart2 = col2.line_chart(x=None, y=None, width=0, height=400)
+chart2.add_rows([1.0]) # Add a row to ensure the chart y-axis starts at 1
+
 
 doors = ['car'] + ['goat'] * 2
 
@@ -22,6 +42,8 @@ def monty_hall(switch):
         
     return doors[final_choice] == 'car'
 
+wins_no_switch = 0
+wins_switch = 0
 
 def count_result(count):
     result_count = []
@@ -31,6 +53,10 @@ def count_result(count):
     result = result_count.count(True) / count * 100
     print(result)
 
-if __name__ == '__main__':
-    count = int(input('How many do you want to repeat the game: '))
-    count_result(count)
+for i in range(count):
+    #simulate one game at a time
+    num_wins_with_switching = monty_hall(True)
+    wins_switch += num_wins_with_switching
+    chart2.add_rows([wins_switch / (i + 1)])
+    
+time.sleep(0.01)
